cba7c8c12821d374c5531672580ffcda91886624
Alwin-Sajan <alwinsajan225@gmail.com>
2024-09-17 20:15:26 +0530

    created layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './index.css';
+import React, { useState } from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import back from './assets/back.png'
 
 const App = () => {
+  const[login,setLogin] =  useState(true);
+  const[preImage,setpreImage] =useState(true);
   return (
-    <div className='w-screen p-8 bg-white'>
-      hey
+    <div className='w-full h-screen relative bg-white'>
+
+      <button className='absolute left-60 top-5 w-16 bg-red-600 z-50' onClick={()=>{setpreImage(!preImage)}}>Switch</button>
+      {preImage?(<img src={back} className='opacity-85 z-10 absolute -mt-24' alt="" />):
+      (<h6></h6>)}
+      
+      {login ? (<HomePage/>) :(
+      
+      <div className='bg-gray-300 h-3/4 w-3/4 m-auto'>
+        hyyy
+      </div>)
+      
+      }
     </div>
   )
 }
