bdc52884e3363a0f33b193759b77039c3cc96f52
Alwin-Sajan <alwinsajan225@gmail.com>
2024-09-17 14:08:01 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='w-screen p-8 bg-white'>
+      hey
+    </div>
+  )
+}
+
+export default App
